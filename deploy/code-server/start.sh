fe61ed4e642de37c311c9229036ad0cc31921b49
Bernhard Boser <bernhard.e.boser@gmail.com>
2024-08-07 18:10:40 -0700

    fix PATH and install libraries

--- deploy/code-server/start.sh
+++ deploy/code-server/start.sh
@@ -5,6 +5,12 @@ if ! grep -q `hostname` /etc/hosts; then
     bash -c 'echo "127.0.0.1" `hostname` >> /etc/hosts'
 fi
 
+cat <<EOT >> /config/.bashrc
+alias python=/usr/bin/python3
+alias pip=/usr/bin/pip3
+export PATH=\$PATH:/usr/bin
+EOT
+
 # Note: /config/custom-cont-init.d executed (if it exists) when code-server starts
 
 sleep infinity
