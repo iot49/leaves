e0c23616745a40417ee4ef4570a35fdd1c1ccb5e
Bernhard Boser <bernhard.e.boser@gmail.com>
2024-08-24 15:45:45 -0700

    leaf docker

---
+++ deploy/leaf/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# running as root!
+
+# credentials used by certbot
+cat > /home/cloudflare/credentials <<EOF
+dns_cloudflare_api_token = "${CF_API_TOKEN}"
+EOF
+
+chmod 600 /home/cloudflare/credentials
+
+# we need access to certificates ...
+chmod a+rx /home/letsencrypt/live
+chmod a+rx /home/letsencrypt/archive
+
+# do we need this? we run from repo ...
+# fix ownership and permissions to edit files with code-server
+chown -R app:app /home/config
+chown -R app:app /home/repo
+chmod a+w /home/homeassistant/*.yaml  # homeassistant runs as root ...
+
+if [[ ${ENVIRONMENT} == "prod" ]]; then
+    # run from docker image
+    setguid app python leaf/main.py
+else
+    # run from repo (setup in editor with rye)
+    (cd /home/repo/backend; setguid app python leaf/main.py)
+fi
+
+sleep infinity
+
